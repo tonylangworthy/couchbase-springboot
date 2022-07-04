5a3400e592c33174c6f226ae3e81ea59e2367b79
Langworthy <tony@webbdealer.com>
2022-07-03 19:14:36 -0500

    TWL - Removed constructor and added Lombok @RequiredArgsConstructor, removed test endpoint.

--- src/main/java/com/example/couchbasedemo/controller/StudentController.java
+++ src/main/java/com/example/couchbasedemo/controller/StudentController.java
@@ -1,9 +1,9 @@
 package com.example.couchbasedemo.controller;
 
-import com.couchbase.client.java.Scope;
 import com.example.couchbasedemo.dto.StudentRecordDto;
 import com.example.couchbasedemo.model.StudentRecord;
 import com.example.couchbasedemo.service.StudentService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,18 +15,10 @@ import java.util.List;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class StudentController {
 
     private final StudentService studentService;
-
-    public StudentController(StudentService studentService) {
-        this.studentService = studentService;
-    }
-
-    @GetMapping("/scope")
-    public String getScope(final Scope scope) {
-        return scope.bucketName();
-    }
 
     @GetMapping("/students")
     public List<StudentRecord> listAllStudents() {
