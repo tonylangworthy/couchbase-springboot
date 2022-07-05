55c7005aa88810f3606e1a591f906986c3839dc3
Kent Hervey <kent.hervey@gmail.com>
2022-07-04 21:25:07 -0500

    updated for more Baeldung features

--- src/main/java/com/example/couchbasedemo/service/CourseService.java
+++ src/main/java/com/example/couchbasedemo/service/CourseService.java
@@ -3,11 +3,13 @@ package com.example.couchbasedemo.service;
 import com.example.couchbasedemo.model.CourseRecord;
 import com.example.couchbasedemo.repository.CourseRecordRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Qualifier("CourseService")
 @RequiredArgsConstructor
 public class CourseService {
 
--- src/main/java/com/example/couchbasedemo/service/StudentService.java
+++ src/main/java/com/example/couchbasedemo/service/StudentService.java
@@ -3,12 +3,14 @@ package com.example.couchbasedemo.service;
 import com.example.couchbasedemo.model.StudentRecord;
 import com.example.couchbasedemo.repository.StudentRecordRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Qualifier("StudentService")
 public class StudentService {
 
     private final StudentRecordRepository studentRecordRepository;
