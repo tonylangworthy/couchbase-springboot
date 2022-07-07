7577e3b0893f63bef4a15ef77c543c01ba0fa63f
Tony Langworthy <tonylangworthy@employbridge.com>
2022-07-07 09:54:50 -0500

    TWL - Added @AllArgsConstructor to be able to create a class without using setters.

--- src/main/java/com/example/couchbasedemo/dto/StudentRecordDto.java
+++ src/main/java/com/example/couchbasedemo/dto/StudentRecordDto.java
@@ -1,10 +1,12 @@
 package com.example.couchbasedemo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class StudentRecordDto {
 
     private String id;
