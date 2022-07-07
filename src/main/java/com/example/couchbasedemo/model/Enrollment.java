69b37c9129e0a12a6986cfc2f79bd9146c1949d6
Tony Langworthy <langworthytech@gmail.com>
2022-07-07 16:45:32 -0500

    TWL - Removed default constructor

--- src/main/java/com/example/couchbasedemo/model/Enrollment.java
+++ src/main/java/com/example/couchbasedemo/model/Enrollment.java
@@ -1,23 +1,15 @@
 package com.example.couchbasedemo.model;
 
-import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.couchbase.core.mapping.Document;
-import org.springframework.data.couchbase.core.mapping.Field;
 
 @Data
 @AllArgsConstructor
 public class Enrollment {
 
-    private String id;
+    private String courseId;
 
     private String dateEnrolled;
 
     private String dateCompleted;
-
-    public Enrollment() {
-    }
-
 }
